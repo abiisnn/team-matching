689c8fe55ffd5f6c33c0b0be72f771cf3b0a0143
shadowmimo <snakegraison@gmail.com>
2019-12-06 09:32:18 -0600

    skills ajax

--- Front/templates/identity/campos.js
+++ Front/templates/identity/campos.js
@@ -1,14 +1,72 @@
+var skillsList=["teamwork","adaptability","problemsolving", "creativity", "presentation skills", "interpersonal skills" , "algorithms skills","wireless comunications","artificial inteligence","natural language processing"];
+
 function crearCampos() {
 	var n = document.getElementById("skills").value;
 	console.log("Creo " + n + " campos");
 	$('#putFields').html('');
 	for(var i = 0; i < parseInt(n); i++) {
 		var res = '<div class="field">';
-	    res += '<input type="text" name="idSkill_' + i + '" id="skill_' + i + '" placeholder="Skill" />'
+	    res += '<input type="text" value="'+skillsList[i]+'" name="idSkill[]' + '" id="skill_' + i + '" placeholder="Skill" />'
         res += '</div>';
         $('#putFields').append(res);
-	}	
+	}
 }
+$( document ).ready(function() {
+ $("#skills").val(skillsList.length);
+});
 
 
+function cargaFormulario(){
+  var idG=$('#idGrupo').val();
+	var skills = [];
+	$("input[name='idSkill[]']").each(function() {
+			skills.push($(this).val());
+	});
+	var url= "http://10.100.66.182:8000/saveOptions?"+idG+"-";
+	for(var i=0; i<skills.length-1; i++)
+		url+= (skills[i].replace(/ /g, "_")+',');
+	url+= (skills[skills.length-1].replace(/ /g, "_"));
+	console.log(url);
+  $.ajax({
+   url:url, //?id
+   method:"GET",
+   data: null,
+   dataType: "text",
+   success:function(data){
+       alert(data);
+   }
 
+  }).fail( function( jqXHR, textStatus, errorThrown ) {
+
+  if (jqXHR.status === 0) {
+
+    alert('Not connect: Verify Network.');
+
+  } else if (jqXHR.status == 404) {
+
+    alert('Requested page not found [404]');
+
+  } else if (jqXHR.status == 500) {
+
+    alert('Internal Server Error [500].');
+
+  } else if (textStatus === 'parsererror') {
+
+    alert('Requested JSON parse failed.');
+
+  } else if (textStatus === 'timeout') {
+
+    alert('Time out error.');
+
+  } else if (textStatus === 'abort') {
+
+    alert('Ajax request aborted.');
+
+  } else {
+
+    alert('Uncaught Error: ' + jqXHR.responseText);
+
+  }
+
+});
+}
